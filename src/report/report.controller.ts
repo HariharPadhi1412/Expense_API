8cb655fa73ea5d09a5a6429b0cfee31617e0a279
Harihar Padhi <39830449+1515236544hit@users.noreply.github.com>
2023-09-25 20:55:01 +0530

    moved the controllers & services to their and  own folder SOP.

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,4 +1,73 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+} from '@nestjs/common/decorators';
+import { ReportType } from 'src/data';
+import { ReportService } from './report.service';
+import { ParseEnumPipe, ParseUUIDPipe } from '@nestjs/common';
+import {
+  ReportDTO,
+  ReportResponseDTO,
+  UpdateReportDTO,
+} from 'src/dtos/report.dto';
 
-@Controller('report')
-export class ReportController {}
+@Controller('report/:type')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Get()
+  getAllReport(
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+  ): ReportResponseDTO[] {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return this.reportService.getAllReport(reportType);
+  }
+
+  @Get(':id')
+  getReportByID(
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string,
+  ): ReportResponseDTO {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return this.reportService.getReportByID(reportType, id);
+  }
+
+  @Post()
+  createReport(
+    @Body() { amount, source }: ReportDTO,
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+  ): ReportResponseDTO {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return this.reportService.createReport(reportType, { amount, source });
+  }
+
+  @Put(':id')
+  updateReport(
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() body: UpdateReportDTO,
+  ): ReportResponseDTO {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return this.reportService.updateReport(reportType, id, body);
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  deleteReport(@Param('id', ParseUUIDPipe) id: string) {
+    return this.reportService.deleteReport(id);
+  }
+}
